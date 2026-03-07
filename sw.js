e0be4e10c128c87805add32df605bee98e9e8926
chrstopher03 <160043279+chrstopher03@users.noreply.github.com>
2026-03-07 14:12:57 -0600

    ya

--- sw.js
+++ sw.js
@@ -7,31 +7,43 @@ const FILES = [
 "23.jpeg"
 ];
 
-self.addEventListener("install", event=>{
+self.addEventListener("install", event => {
+
   self.skipWaiting();
 
   event.waitUntil(
-    caches.open(CACHE)
-    .then(cache=>cache.addAll(FILES))
+    caches.open(CACHE).then(cache=>{
+      return cache.addAll(FILES);
+    })
   );
+
 });
 
-self.addEventListener("activate", event=>{
+self.addEventListener("activate", event => {
+
   event.waitUntil(
     caches.keys().then(keys=>{
       return Promise.all(
-        keys.filter(key=>key !== CACHE)
-        .map(key=>caches.delete(key))
+        keys.map(key=>{
+          if(key !== CACHE){
+            return caches.delete(key);
+          }
+        })
       );
     })
   );
 
   self.clients.claim();
+
 });
 
-self.addEventListener("fetch", event=>{
+self.addEventListener("fetch", event => {
+
   event.respondWith(
     caches.match(event.request)
-    .then(res=> res || fetch(event.request))
+    .then(response=>{
+      return response || fetch(event.request);
+    })
   );
+
 });
