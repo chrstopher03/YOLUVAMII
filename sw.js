b4a8bc9f16268366a3a8e2686d60a60ecc9ad00d
chrstopher03 <160043279+chrstopher03@users.noreply.github.com>
2026-03-07 19:25:54 -0600

    ya

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "yoluvami-cache-v4";
+const CACHE = "yoluvami-cache-v5";
 
 const FILES = [
 "/",
@@ -9,41 +9,58 @@ const FILES = [
 
 self.addEventListener("install", event => {
 
-  self.skipWaiting();
+self.skipWaiting();
 
-  event.waitUntil(
-    caches.open(CACHE).then(cache=>{
-      return cache.addAll(FILES);
-    })
-  );
+event.waitUntil(
+caches.open(CACHE).then(cache=>{
+return cache.addAll(FILES);
+})
+);
 
 });
 
 self.addEventListener("activate", event => {
 
-  event.waitUntil(
-    caches.keys().then(keys=>{
-      return Promise.all(
-        keys.map(key=>{
-          if(key !== CACHE){
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+event.waitUntil(
+caches.keys().then(keys=>{
+return Promise.all(
+keys.map(key=>{
+if(key !== CACHE){
+return caches.delete(key);
+}
+})
+);
+})
+);
 
-  self.clients.claim();
+self.clients.claim();
 
 });
 
 self.addEventListener("fetch", event => {
 
-  event.respondWith(
-    caches.match(event.request)
-    .then(response=>{
-      return response || fetch(event.request);
-    })
-  );
+event.respondWith(
+caches.match(event.request)
+.then(response=>{
+return response || fetch(event.request);
+})
+);
+
+});
+
+/* PUSH NOTIFICATION */
+
+self.addEventListener("push", event=>{
+
+const data = event.data?.text() || "Nueva notificación";
+
+event.waitUntil(
+
+self.registration.showNotification("YOLUVAMI",{
+body:data,
+icon:"23.jpeg"
+})
+
+);
 
 });
